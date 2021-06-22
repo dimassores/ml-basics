04516d581cf99e3944da1bbef9134a8f66919759
Dimas <dimas.lima@corp.globo.com>
2021-06-22 09:09:15 -0300

    fashion exploration

--- utils.py
+++ utils.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 
-def plot_2d_embedding(X, y, size, title=None):
+def plot_2d_embedding(X, y, labels, size, title=None):
+    
     x_min, x_max = np.min(X, 0), np.max(X, 0)
     X = (X - x_min) / (x_max - x_min)
 
     plt.figure(figsize=(15, 10))
     ax = plt.subplot(111)
     for i in range(X.shape[0]):
-        plt.text(X[i, 0], X[i, 1], str(y[i]),
+        plt.text(X[i, 0], X[i, 1], str(labels[i]),
                  color=plt.cm.Set1(y[i] / 10.),
                  fontdict={'weight': 'bold', 'size': size})
         
